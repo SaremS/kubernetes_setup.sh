c5ceee19033432617b860e77b3b4672c36a8d3b5
Sarem Seitz <sarem.seitz@gmail.com>
2024-04-28 13:39:18 +0200

    Update 03_init_kubeadm.sh

--- 03_init_kubeadm.sh
+++ 03_init_kubeadm.sh
@@ -1,5 +1,5 @@
 sudo rm /etc/containerd/config.toml
 sudo systemctl restart containerd
-kubeadm init --apiserver-cert-extra-sans=10.0.0.3 --apiserver-advertise-address=10.0.0.3
+kubeadm init --apiserver-cert-extra-sans=10.0.0.3 --apiserver-advertise-address=10.0.0.3 --pod-network-cidr=10.244.0.0/16
 
 kubeadm token create --print-join-command > cluster_join_command.sh
