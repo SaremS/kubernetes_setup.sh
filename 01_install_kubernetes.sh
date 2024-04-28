770cc7f444f35b4cb0db1f62c9d2058aedad80a2
Sarem Seitz <sarem.seitz@gmail.com>
2024-04-28 13:38:49 +0200

    Update 01_install_kubernetes.sh

--- 01_install_kubernetes.sh
+++ 01_install_kubernetes.sh
@@ -28,6 +28,5 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# enable kubelet
-sudo systemctl enable --now kubelet
+swapoff –a
 
